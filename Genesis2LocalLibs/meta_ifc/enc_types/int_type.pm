bb8f8523ab285bfaaddcdf133872638456589018
steveri <steveri@steveri.com>
2017-02-16 08:26:01 -0800

    Deleted dir that is not (yet?) part of release.

--- Genesis2LocalLibs/meta_ifc/enc_types/int_type.pm
+++
@@ -1,6 +0,0 @@
-#This specifies the integer encoding type. Since encoding class already 
-#implements integer functionality, this file is mostly empty.
-package integer;
-use encoding;
-use strict;
-our @ISA = qw(encoding); #Inherits from signal
