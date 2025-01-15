96050e35632ae5f908dc018d6a2c188be937471a
steveri <steveri@steveri.com>
2025-01-15 11:45:10 -0800

    Update pip/bin/Genesis2.pl to work with either old or new regime maybe

--- pip/genesis2/bin/Genesis2.pl
+++ pip/genesis2/bin/Genesis2.pl
@@ -9,8 +9,13 @@ PERL_LIB_VAR = "PERL5LIB"
 
 # PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$PERL5LIB
 
-GENESIS_HOME = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(genesis2.__file__))),
-                            "Genesis2-src", "Genesis2Tools")
+DOT = os.path.abspath(os.path.dirname(os.path.dirname(genesis2.__file__)))
+GENESIS_HOME = os.path.join(DOT, "Genesis2-src", "Genesis2Tools")
+
+# Guess what things are about to change maybe
+if not os.path.isdir(GENESIS_HOME):
+    GENESIS_HOME = os.path.join(DOT, "Genesis2-src")
+
 os.environ["GENESIS_HOME"] = GENESIS_HOME
 genesis_path = os.path.join(GENESIS_HOME, "bin", "Genesis2.pl")
 perl_path = os.path.join(GENESIS_HOME, "PerlLibs", "ExtrasForOldPerlDistributions")
