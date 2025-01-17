392e230608b72ce393bba0ddfbc87c934a68db8e
steveri <steveri@steveri.com>
2025-01-17 08:28:20 -0800

    Better mechanism for determining old vs. new regime in pip bin script

--- pip/genesis2/bin/Genesis2.pl
+++ pip/genesis2/bin/Genesis2.pl
@@ -9,12 +9,20 @@ PERL_LIB_VAR = "PERL5LIB"
 
 # PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$PERL5LIB
 
-DOT = os.path.abspath(os.path.dirname(os.path.dirname(genesis2.__file__)))
-GENESIS_HOME = os.path.join(DOT, "Genesis2-src", "Genesis2Tools")
-
+# GENESIS_HOME = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(genesis2.__file__))),
+#                             "Genesis2-src", "Genesis2Tools")
+#
 # Guess what things are about to change maybe
-if not os.path.isdir(GENESIS_HOME):
+
+DOT = os.path.abspath(os.path.dirname(os.path.dirname(genesis2.__file__)))
+
+OLD_SCRIPTPATH = os.path.join(DOT, "Genesis2-src", "Genesis2Tools", "bin", "Genesis2.pl")
+NEW_SCRIPTPATH = os.path.join(DOT, "Genesis2-src", "bin", "Genesis2.pl")
+
+if os.path.isfile(NEW_SCRIPTPATH):
     GENESIS_HOME = os.path.join(DOT, "Genesis2-src")
+else:
+    GENESIS_HOME = os.path.join(DOT, "Genesis2-src", "Genesis2Tools")
 
 os.environ["GENESIS_HOME"] = GENESIS_HOME
 genesis_path = os.path.join(GENESIS_HOME, "bin", "Genesis2.pl")
