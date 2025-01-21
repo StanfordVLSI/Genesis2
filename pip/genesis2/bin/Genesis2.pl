a1e2c816cf58921389447e656c783f7a6a1eaf1e
steveri <steveri@steveri.com>
2025-01-21 11:47:10 -0800

    increased robustness for varying PERL5LIB locations

--- pip/genesis2/bin/Genesis2.pl
+++ pip/genesis2/bin/Genesis2.pl
@@ -7,7 +7,8 @@ import genesis2
 
 PERL_LIB_VAR = "PERL5LIB"
 
-# PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$PERL5LIB
+# OLD: PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$PERL5LIB
+# NEW: PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$GENESIS_HOME/PerlLibs:$PERL5LIB
 
 # GENESIS_HOME = os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(genesis2.__file__))),
 #                             "Genesis2-src", "Genesis2Tools")
@@ -26,7 +27,12 @@ else:
 
 os.environ["GENESIS_HOME"] = GENESIS_HOME
 genesis_path = os.path.join(GENESIS_HOME, "bin", "Genesis2.pl")
-perl_path = os.path.join(GENESIS_HOME, "PerlLibs", "ExtrasForOldPerlDistributions")
+
+# @grg is changing Genesis2.pl to use libs in top-level "PerlLibs" directory
+perl_path1 = os.path.join(GENESIS_HOME, "PerlLibs", "ExtrasForOldPerlDistributions")
+perl_path2 = os.path.join(GENESIS_HOME, "PerlLibs")
+perl_path = perl_path1 + ":" + perl_path2
+
 args = " ".join(sys.argv[1:])
 r = os.system(PERL_LIB_VAR + "=" + perl_path + ":${" + PERL_LIB_VAR + "} " + genesis_path + " " + args)
 if r != 0:
--- pip/genesis2/setup.py
+++ pip/genesis2/setup.py
@@ -52,7 +52,7 @@ class Genesis2Build(build_ext):
 
 setup(
     name='genesis2',
-    version='0.0.8',
+    version='0.0.9',
     packages=[
         "genesis2"
     ],
