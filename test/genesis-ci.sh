8eebc80787ddf06f8b6691e46e2a2f2a28899d54
steveri <steveri@steveri.com>
2025-03-18 07:12:02 -0700

    embed failure path for testing

--- test/genesis-ci.sh
+++ test/genesis-ci.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Uncomment to test failure path
+TEST_FAILURE_PATH=true
+
 USAGE="
   $0 <commit>
 
@@ -14,7 +17,7 @@ USAGE="
 if [ "$1" == "--help" ]; then echo "$USAGE"; exit; fi
 
 # What does this script do?
-# - verifies that tmp-garnet.v[01] does not exist already
+# - verifies that compare-dirs tmp-garnet.d[01] do not exist already
 # - launches a container based on garnet:latest
 # -- pip-installs latest genesis2 (maybe not necessary no more?)
 # -- builds gold verilog tmp-garnet.v0 using master branch
@@ -86,7 +89,30 @@ GROUP "TEST-BUILD ($commit) > tmp-garnet.v1"
 printf "\nINFO Build test-model verilog using Genesis2 branch '$commit'\n"
 REPO=/aha/lib/python3.8/site-packages/Genesis2-src
 dexec "set -x; cd $REPO; git pull; git checkout -fq $commit" || exit 13
+if [ "$TEST_FAILURE_PATH" ]; then
+    # Inject a fault in Genesis2.pl
+    # FAULT INJECTION 
+    # FAULT INJECTION print("FOO Attempting fault injection")
+    # FAULT INJECTION if os.path.isfile("/aha/garnet/genesis_verif/jtag.sv"):
+    # FAULT INJECTION   print("FOO injecting error in /aha/garnet/genesis_verif/jtag.sv")
+    # FAULT INJECTION   r2 = os.system("set -x; bsed -i.bak 's/addr = 0/addr = 13/' /aha/garnet/genesis_verif/jtag.sv")
+    # FAULT INJECTION   print("FOO r2 = "); print (r2)
+    # FAULT INJECTION   if r2 != 0: sys.exit(13)
+    # FAULT INJECTION   print("FOO injection SUCCESSFULL...???")
+    # FAULT INJECTION 
+    fault=$(egrep '^    # FAULT INJECTION' $0 | sed 's/.*INJECTION //' > /tmp/tmp$$)
+    dexec "cp /aha/bin/Genesis2.pl /aha/bin/Genesis2.pl0"
+    docker cp /tmp/tmp$$ $container:/tmp; # /bin/rm /tmp/tmp$$
+    dexec "cat /tmp/tmp$$ >> /aha/bin/Genesis2.pl"
+    dexec "diff /aha/bin/Genesis2.pl0 /aha/bin/Genesis2.pl"
+fi
 dexec "$build_garnet"
+if [ "$TEST_FAILURE_PATH" ]; then
+    set -x  # Show injected fault
+    dexec 'set -x; ls -l /aha/garnet/genesis_verif/'
+    dexec 'set -x; diff /aha/garnet/genesis_verif/jtag.{sv.bak,sv}'
+    set +x
+fi
 docker cp ${container}:/aha/garnet/garnet.v tmp-garnet.v1
 dexec 'cd /aha/garnet; make clean' >& /dev/null  # Clean up your mess, ignore errors :(
 ENDGROUP
@@ -112,6 +138,11 @@ echo "versus    `vcompare $f2 | wc -l` lines of $f2"
 printf "\n"
 
 echo "diff $f1 $f2"
+diff $f1 $f2
+
+
+
+
 ndiffs=`vcompare $f1 $f2 | wc -l`
 
 if [ "$ndiffs" != "0" ]; then
