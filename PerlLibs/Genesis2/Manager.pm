3dbd14d590a6724f8710dc8d4e1af050e564f370
steveri <steveri@steveri.com>
2025-05-17 08:13:33 -0700

    works

--- PerlLibs/Genesis2/Manager.pm
+++ PerlLibs/Genesis2/Manager.pm
@@ -946,13 +946,12 @@ sub find_file_safe{
           last; # got one, so exit the loop
         }
 
-# 	$filefound = 1 if (-e "${dir}/${file}");
-# 	if ($filefound) {
-# 	    # Change file path so it is now absolute.
-# 	    $file = "${dir}/${file}";
-# 	    last; # got one, so exit the loop
-# 	}
-
+	$filefound = 1 if (-e "${dir}/${file}");
+	if ($filefound) {
+	    # Change file path so it is now absolute.
+	    $file = "${dir}/${file}";
+	    last; # got one, so exit the loop
+	}
     }
   }
 
